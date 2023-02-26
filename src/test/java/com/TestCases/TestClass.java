dedb0b87f9e33f0fd532121bec517795ab3f7e15
ss143ss <ssymail420@gmail.com>
2023-02-26 16:34:44 +0530

    css

--- src/test/java/com/TestCases/TestClass.java
+++ src/test/java/com/TestCases/TestClass.java
@@ -12,8 +12,9 @@ public class TestClass {
 	public void TC1() {
 		WebDriverManager.chromedriver().setup();;
 		WebDriver driver = new ChromeDriver();
-
-		driver.get("https://www.amazon.com");  
+		driver.get("https://www.amazon.com"); 
+		//////////////////////////cdcptfgnmc
+		 
 	}
 
 
