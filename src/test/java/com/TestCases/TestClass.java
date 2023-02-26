e85299ab8f05746ebeab1c96a44dbe43640aa725
ss143ss <ssymail420@gmail.com>
2023-02-26 15:55:17 +0530

    tes

---
+++ src/test/java/com/TestCases/TestClass.java
@@ -0,0 +1,17 @@
+package com.TestCases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TestClass {
+
+	@Test
+	public void TC1() {
+		WebDriverManager.chromedriver().setup();;
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.amazon.com");
+	}
+}
